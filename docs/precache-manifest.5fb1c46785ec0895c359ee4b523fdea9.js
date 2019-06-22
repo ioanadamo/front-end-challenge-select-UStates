fc7e63cbd95af88dae6e711d681580a10acdf69f
ioanadamo <daniela932910@gmail.com>
2019-06-22 19:54:39 +0200

    add build

---
+++ docs/precache-manifest.5fb1c46785ec0895c359ee4b523fdea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b87d5b03c516e9733cd50508faa655",
+    "url": "./index.html"
+  },
+  {
+    "revision": "bf1d78faeede2cca72be",
+    "url": "./static/css/main.437e9166.chunk.css"
+  },
+  {
+    "revision": "ee46273d705463213bfe",
+    "url": "./static/js/2.d3f673b0.chunk.js"
+  },
+  {
+    "revision": "bf1d78faeede2cca72be",
+    "url": "./static/js/main.882543c7.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  },
+  {
+    "revision": "b2abf0a7c07385822935d49c353ade74",
+    "url": "./static/media/xImage.b2abf0a7.png"
+  }
+]);
